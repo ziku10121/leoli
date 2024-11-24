b20d529ea9174123d3237d91c28417cafee578e3
LeoLi <ziku10121@gmail.com>
2024-11-24 15:41:54 +0800

    update

---
+++ assets/404-n-I1fTzM.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o as t,a as c}from"./index-6PmDtibf.js";const o={},_={class:"flex justify-center"},a=c("div",{class:"text-46"},"404",-1),n=[a];function r(d,i){return t(),s("div",_,n)}const l=e(o,[["render",r]]);export{l as default};
