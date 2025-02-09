45f70b87793d57a0d0bff88eeeb0ffe74d18c49b
LeoLi <ziku10121@gmail.com>
2025-02-09 18:43:28 +0800

    update path

--- assets/404-CLCRUdHf.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as s,o as t,a as c}from"./index-C3Ms-rhh.js";const o={},_={class:"flex justify-center"},a=c("div",{class:"text-46"},"404",-1),n=[a];function r(d,i){return t(),s("div",_,n)}const l=e(o,[["render",r]]);export{l as default};
